1e6ed7dfab29244b824d5a8c562ba726c7035e35
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-14 21:02:59 -0600

    personalize the initial bot response to a new issue

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Probot } from "probot";
 export default (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: `Thanks for opening this issue! Please join us on Telegram to discuss this issue in greater detail! \n [![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/AIhawkCommunity)`,
+      body: `Hi there @${context.payload.sender.login}, thanks for opening this issue! Please join us on Telegram to discuss this issue in greater detail! \n\n [![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/AIhawkCommunity)`,
     });
     await context.octokit.issues.createComment(issueComment);
   });

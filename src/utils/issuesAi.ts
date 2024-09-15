7994f97fa9debef470565e8fbaccc61ed9b8a0f2
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-15 13:31:58 -0600

    working implementation

--- src/utils/issuesAi.ts
+++ src/utils/issuesAi.ts
@@ -1,4 +1,4 @@
-// A "High Conversation" AI Model for comment analysis
+// A "High Conversation" AI Model for comment analysis and responses
 // i.e. gpt-4o-mini for cost savings
 import OpenAi from 'openai';
 import type { OpenAI } from 'openai/src/index.js';
@@ -11,7 +11,12 @@ export async function mainTest(context: IssueContextObj | any) {
     messages: [{ role: 'user', content: `Say this is a test of @${context.payload.sender.login}'s comment saying ${context.payload.comment.body}...` }],
     model: 'gpt-4o-mini',
   };
+  
   const chatCompletion: OpenAI.Chat.ChatCompletion = await issuesAiClient.chat.completions.create(params);
-  return chatCompletion
-};
+  // console.log(chatCompletion.choices[0].message)
+  
+  // return chatCompletion.choices[0].message.content
 
+  const aiResponse = chatCompletion.choices.map(n=>n.message.content).join(`\n\n`);
+  return aiResponse
+};

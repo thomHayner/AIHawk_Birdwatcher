5318910db461632fb0fce80c6e2356de4d7b64a4
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-15 16:46:56 -0600

    add skeleton code for future scenarios

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import issueCommentCreated from "./webhooks/issue_comments.js";
 
 export default (app: Probot) => {
 
-  // When a new issue is opened
+  // When a new issue is opened:
   app.on("issues.opened", async (context) => {
     // console.log(context)
 
     const issueComment = await issueOpened(context)
     await context.octokit.issues.createComment(issueComment);
   });
+
+  // When an existing issue's originating comment is edited:
+  
 
   // When an existing issue is commented on
   app.on("issue_comment.created", async (context) => {
@@ -25,7 +28,10 @@ export default (app: Probot) => {
     await context.octokit.issues.createComment(issueComment);
   });
 
-  // When a new Pull Request is opened
+  //When an existing comment (not the originating comment) is edited:
+
+
+  // When a new Pull Request is opened:
 
 
   // For more information on building apps:

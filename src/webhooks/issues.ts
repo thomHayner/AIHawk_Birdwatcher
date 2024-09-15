321a96da4b38085a47ea6cee7d61464602d0c24d
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-15 16:56:11 -0600

    rename test function, improve response working

--- src/webhooks/issues.ts
+++ src/webhooks/issues.ts
@@ -1,17 +1,18 @@
-import { mainTest } from "../utils/issuesAi.js";
+import { issueOpenedCompletion } from "../utils/issuesAi.js";
 
 export default async function issueOpened(context: any): Promise<any> {
-  // Initial greeting, direct users to Telegram
-  const greeting = `Hi there @${context.payload.sender.login}, thanks for opening this issue! Please join us on Telegram to discuss this issue in greater detail! \n\n [![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/AIhawkCommunity)`;
+  // Greet the user and direct them to Telegram
+  const greeting = `Hi there @${context.payload.sender.login}, thanks for 
+  opening this issue! Please join us on Telegram to discuss this issue in 
+  greater detail!` + `\n\n` + `[![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/AIhawkCommunity)`;
   
   // ChatGPT check/analyze for duplicate issues, add tags
-  // const issueComment = context.issue({
-  //   body: `${mainTest(context)}`,
-  // });
+  const aiResponseMessage = await issueOpenedCompletion(context);
 
   // Build a complete comment body as a respose to the new issue
+  const responseBody = greeting + `\n\n` + `----- ----- -----` + `\n\n` + aiResponseMessage
   const issueComment = context.issue({
-    body: greeting,
+    body: responseBody,
   });
 
   return await issueComment;

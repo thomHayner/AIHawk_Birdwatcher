bc9d8126f810647a534cc2117cf36b3adf36e21d
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-15 18:34:21 -0600

    cleanup: content spacing and markdown

--- src/webhooks/issues.ts
+++ src/webhooks/issues.ts
@@ -4,14 +4,14 @@ export default async function issueOpened(context: any): Promise<any> {
   // Greet the user and direct them to Telegram
   const greeting = `Hi there @${context.payload.sender.login}, thanks for \
   opening this issue! Please join us on Telegram to discuss this issue in \
-  greater detail!` + `\n\n` + 
+  greater detail!` + `\n\n\n\n` + 
   `[![Telegram](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/AIhawkCommunity)`;
   
   // ChatGPT check/analyze for duplicate issues, add tags
   const aiResponseMessage = await issueOpenedCompletion(context);
 
   // Build a complete comment body as a respose to the new issue
-  const responseBody = greeting + `\n\n` + `----- ----- -----` + `\n\n` + aiResponseMessage;
+  const responseBody = greeting + `\n\n` + `---` + `\n\n` + aiResponseMessage;
 
   const issueComment = context.issue({
     body: responseBody,

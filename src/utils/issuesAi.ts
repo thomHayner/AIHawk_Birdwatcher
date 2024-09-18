a94e314b1cc48420d045ba82b4d0f54a5254540d
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-17 18:21:07 -0600

    split prompt into smaller messages for wider adaptability

--- src/utils/issuesAi.ts
+++ src/utils/issuesAi.ts
@@ -2,38 +2,33 @@
 // i.e. gpt-4o-mini for cost savings
 import OpenAi from 'openai';
 import type { OpenAI } from 'openai/src/index.js';
-import type { IssueContextObj } from './aiTypes.js';
+import type { IssueContextObj } from './aiObjectTyping.js';
 
 const issuesAiClient = new OpenAi({ apiKey: process.env.OPENAI_API_KEY });
 
-const systemPrompt:string = `You are an intelligent Software Engineer working as a GitHub Repository Maintainer on an open source project.`
+const systemPrompt:string = `\
+  You are an intelligent Software Engineer working as a GitHub Repository Maintainer on an open source project.\n
+  \n
+  You are responsible for:\n
+    - Management and moderation of the Issues, Discussions and Pull Requests Features.\n
+    - Code reviews\
+`
+const nextTask:string = `\
+  Your next task involves the Issues feature, which GitHub describes in the following way: "Issues integrate lightweight task tracking into your repository. Keep projects on track with issue labels and milestones, and reference them in commit messages."\n
+  These are your responsibilities relating to Issues:\n
+    - You will analyze new issues and triage them.\n
+    - If an issue is missing key information, you will ask follow up questions.\n
+    - You will apply appropriate labels to issues.\n
+    - You will compare new issues to existing issues (both open and closed) to determine if the new issue is a duplicate.\n
+    - If an issue is a duplicate you will add the duplicate label and reference the new issue with a comment in the existing issue's conversation and then you will close the new issue.\n
+    - You will look for inappropriate language; if an issue or comment contains questionable content you will hide the message from public view and notify human moderators for follow up.\n
+    - You will close stale issues after a predetermined length of time.\
+`
 
-const taskAssignedByUser = `Your tasks include:\
-        - management and moderation of the Issues, Discussions and Pull reviews Features\
-        - code reviews, automated workflows, CI/CD and testing\
-        \n\n
-        the Issues feature, which GitHub describes in the following way: "Issues integrate lightweight task tracking into your repository. Keep projects on track with issue labels and milestones, and reference them in commit messages."
-        - You will read issues to analyze and triage them.
-        - If an issue is missing key information, you will ask follow up questions.
-        - You will apply appropriate labels to issues.
-        - You will compare new issues to existing or previous issues and determine if the new issue is a duplicate.
-        - If an issue is a duplicate you will add the duplicate label and reference the new issue with a comment in the existing issue's conversation and then you will close the new issue.
-        - You will look for inappropriate language; if an issue or comment contains questionable content you will hide the message from public view and notify human moderators for follow up.
-        - You will close stale issues after a predetermined length of time.
-        \n\n
-        You interact with the repository through a bot that is limited to pre-programmed interactions with the GitHub API. For now you will simply reply with a single response, in the future you will have more functionality.
-        \n\n
-        The next message will contain a new issue`;
-
-        `Maintainers need to wear many hats, tasks include:
-        - project management,
-        - troubleshooting and triaging,
-        - managing community contributions,
-        - dependency management,
-        - git ops,
-        - and ensuring the overall health of the project.`
+const specificTask:string = `The next message will contain a new issue, respond by telling me what follow up questions you would ask and which labels you would apply.`
 
 export async function issueOpenedCompletion(context:IssueContextObj | any) {
+  const issue = context.payload.issue;
   const params:OpenAI.Chat.ChatCompletionCreateParams = {
     messages: [
       {
@@ -42,7 +37,15 @@ export async function issueOpenedCompletion(context:IssueContextObj | any) {
       },
       {
         role: 'user',
-        content: `${context.payload.issue.title}: ${context.payload.issue.body}`,
+        content: nextTask,
+      },
+      {
+        role: 'user',
+        content: specificTask,
+      },
+      {
+        role: 'user',
+        content: `${issue.labels.length > 0 ? 'Labels: ' + issue.labels.map((n:any) => n.name + ', ') + ' ' : ''}${issue.title}: ${issue.body}`,
       },
     ],
     model: 'gpt-4o-mini',
@@ -53,6 +56,7 @@ export async function issueOpenedCompletion(context:IssueContextObj | any) {
 };
 
 export async function issueCommentCompletion(context:IssueContextObj | any, messages:any) {
+  const issue = context.payload.issue;
   const params:OpenAI.Chat.ChatCompletionCreateParams = {
     messages: [
       {
@@ -61,7 +65,15 @@ export async function issueCommentCompletion(context:IssueContextObj | any, mess
       },
       {
         role: 'user',
-        content: `${context.payload.issue.title}: ${context.payload.issue.body}`,
+        content: nextTask,
+      },
+      {
+        role: 'user',
+        content: specificTask,
+      },
+      {
+        role: 'user',
+        content: `${issue.labels.length > 0 ? 'Labels: ' + issue.labels.map((n:any) => n.name + ', ') + ' ' : ''}${issue.title}: ${issue.body}`,
       },
       ...messages
     ],
@@ -70,4 +82,7 @@ export async function issueCommentCompletion(context:IssueContextObj | any, mess
   const chatCompletion:OpenAI.Chat.ChatCompletion = await issuesAiClient.chat.completions.create(params);
   const aiResponse:string = chatCompletion.choices.map(n=>n.message.content).join(`\n`);
   return aiResponse
+
+  // There needs to be an abort here, somthing that checks if the conversation should be over and stops responding
+  // There should also be soemthing that checks to make sure it is appropriate to respond at all, for instance, if a user specifically sends a message to someone else the bot should not respond
 };

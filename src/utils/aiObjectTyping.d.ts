8c4bd47175a6a7f31a50388a24c310d136257447
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-19 16:56:32 -0600

    update types

--- src/utils/aiObjectTyping.d.ts
+++ src/utils/aiObjectTyping.d.ts
@@ -1,6 +1,14 @@
+import { Messages } from "openai/resources/beta/threads/messages.mjs";
+
 export type IssueContextObj = {
+  issue(arg0: { issue_number: any; labels: any; }): any;
+  octokit: any;
   payload: {
+    sender: any;
     issue: {
+      number: any;
+      body: any;
+      labels: any;
       sender: {
         login: any;
       };

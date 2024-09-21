5d7a98ece492ece7a909353324b4ca53787224d7
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-21 10:12:46 -0600

    cleanup messy filetree

--- src/utils/codeReviewAi.ts
+++
@@ -1,7 +0,0 @@
-// A "High Logic" AI Model for code analysis
-// i.e. gpt-o1 for high accuracy code reviews and logic analysis
-import OpenAi from 'openai';
-import type { OpenAI } from 'openai/src/index.js';
-import type { IssueContextObj } from './aiObjectTyping.js';
-
-const codeReviewAiClient = new OpenAi({ apiKey: process.env.OPENAI_API_KEY });

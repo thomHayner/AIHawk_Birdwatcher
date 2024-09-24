455798e0a8f03bb3fb2203ebe87a5ab7c6baf1f7
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-24 13:44:52 -0600

    update accordint to docs

--- src/utils/octokit.ts
+++ src/utils/octokit.ts
@@ -1,3 +1,15 @@
-import { ProbotOctokit } from "probot";
+// https://github.com/probot/octokit-auth-probot?tab=readme-ov-file
 
-export const octokit = new ProbotOctokit({ auth: process.env.GITHUB_TOKEN });
+import { Octokit } from "@octokit/core";
+import { createProbotAuth } from "octokit-auth-probot";
+
+const ProbotOctokit = Octokit.defaults({
+  authStrategy: createProbotAuth,
+});
+
+export const octokit = new ProbotOctokit({
+  auth: {
+    appId: process.env.APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+  },
+});

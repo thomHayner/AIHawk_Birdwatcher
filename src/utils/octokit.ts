307361e20e5b076e93a4acb7aeb80aa36cc6aaf6
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-24 16:26:27 -0600

    add client authorization (working)

--- src/utils/octokit.ts
+++ src/utils/octokit.ts
@@ -1,15 +1,17 @@
-// https://github.com/probot/octokit-auth-probot?tab=readme-ov-file
-
-import { Octokit } from "@octokit/core";
+import { ProbotOctokit } from "probot";
 import { createProbotAuth } from "octokit-auth-probot";
 
-const ProbotOctokit = Octokit.defaults({
-  authStrategy: createProbotAuth,
-});
+const privateKey = Buffer.from(`${process.env.PRIVATE_KEY}`, 'base64').toString('ascii');
+const appId = `${process.env.APP_ID}`
 
 export const octokit = new ProbotOctokit({
+
+  authStrategy: createProbotAuth,
   auth: {
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
-  },
+    appId: appId,
+    privateKey: privateKey,
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    installationId: process.env.INSTALLATION_ID
+  }
 });

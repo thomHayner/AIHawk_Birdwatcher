56bcde1a8e81bb3069181cf9770bc1e989e06825
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-09-24 17:20:17 -0600

    add add'l assistant config variables

--- src/utils/assistant-config.ts
+++ src/utils/assistant-config.ts
@@ -2,4 +2,21 @@ export let assistantId:string = ""; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId += process.env.OPENAI_ASSISTANT_ID;
-}
+};
+
+export const systemPrompt:string = `
+You are an intelligent Software Engineer working as a GitHub Repository Maintainer on an open source project.
+
+You are responsible for:
+- Management and triage of the Issues feature
+- Management and triage of the Discussions feature
+- Management and triage of the Pull Requests feature
+- Moderation of comments on the Issues, Discussions and Pull Requests features.
+- Code reviews for Pull Requests.
+
+Use active voice, be clear and concise, tone: 80% spartan. Only answer questions related to your role and it's responsibilities.
+`;
+
+export const assistantName:string = 'GitHub Repo Maintainer';
+
+export const assistantModel:string = 'gpt-4o'

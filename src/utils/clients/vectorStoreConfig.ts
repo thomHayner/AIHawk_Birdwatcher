656b1a6bbf722dcae9bbb33f6872051527928d8e
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-02 12:46:21 -0600

    implement findOrCreateVectorStore()

--- src/utils/clients/vectorStoreConfig.ts
+++ src/utils/clients/vectorStoreConfig.ts
@@ -1,5 +1,24 @@
-export let vectorStoreId:string = ""; // set your vector store ID here
+import { openai } from "./openai.js";
 
-if (vectorStoreId === "") {
-  vectorStoreId += process.env.OPENAI_VECTOR_STORE_ID;
+let vectorStoreId:string|undefined = process.env.OPENAI_VECTOR_STORE_ID; // set your vector store ID here
+
+
+export default async function findOrCreateVectorStore():Promise<string> {
+  if (vectorStoreId && vectorStoreId !== "") {
+    // Use the provided vectorStoreId
+    console.log("vectorStoreId:", vectorStoreId);
+  } else if (vectorStoreId === undefined || vectorStoreId === "") {
+    const vectorStore = await openai.beta.vectorStores.create({
+      name: "Repository Vector Store"
+    });
+
+    // As well as the value to be returned
+    vectorStoreId = vectorStore.id
+    // Then set the value for the OPENAI_ASSISTANT_ID key in the .env file
+    process.env.OPENAI_VECTOR_STORE_ID = vectorStoreId;
+
+    console.log("Vector Store created.");
+    console.log("vectorStoreId:", vectorStoreId);
+  }
+  return vectorStoreId;
 }

65048a6fb22fc4b4fce96e68a0dbb89391c56825
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-02 12:20:47 -0600

    add findOrCreateAgent()

--- src/utils/clients/assistant-config.ts
+++ src/utils/clients/assistant-config.ts
@@ -1,10 +1,8 @@
-export let assistantId:string = ""; // set your assistant ID here
+import { openai } from "../clients/openai.js";
 
-if (assistantId === "") {
-  assistantId += process.env.OPENAI_ASSISTANT_ID;
-};
+const assistantName:string = 'GitHub Repo Maintainer';
 
-export const systemPrompt:string = `
+const systemPrompt:string = `
 You are an intelligent Software Engineer working as a GitHub Repository Maintainer on an open source project.
 
 You are responsible for:
@@ -17,6 +15,39 @@ You are responsible for:
 Use active voice, be clear and concise, tone: 80% spartan. Only answer questions related to your role and it's responsibilities.
 `;
 
-export const assistantName:string = 'GitHub Repo Maintainer';
+const assistantModel:string = 'gpt-4o';
 
-export const assistantModel:string = 'gpt-4o'
+let assistantId:string|undefined = process.env.OPENAI_ASSISTANT_ID; // set your assistant ID here
+
+export default async function findOrCreateAssistant():Promise<string> {
+  if (assistantId && assistantId !== "") {
+    // Use the provided assistantId
+    console.log("assistantId:", assistantId);
+  } else if (assistantId === undefined || assistantId === "") {
+    // Shell prompt at runtime asking user if they want to add an existing assistantId or create a new assistant
+    // TODO: develop shell script for asking question
+    // const askQuestion = await askQuestionScript
+
+    // If they want to add an existing assistantId
+    // if (askQuestion === "n" || askQuestion === "no" || askQuestion === "N" || askQuestion === "No" || askQuestion === "NO") {
+      // TODO: develop the shell scripts for adding an existing id
+      // TODO: then set the value OPENAI_ASSISTANT_ID key in the .env file to the user's terminal input
+    // } else if (askQuestion === "y" || askQuestion === "yes" || askQuestion === "Y" || askQuestion === "Yes" || askQuestion === "YES") {}
+
+    const assistant = await openai.beta.assistants.create({
+      name: assistantName,
+      instructions: systemPrompt,
+      tools: [{ type: "code_interpreter" }, {"type": "file_search"}],
+      model: assistantModel,
+    });
+
+    // As well as the value to be returned
+    assistantId = assistant.id
+    // Then set the value for the OPENAI_ASSISTANT_ID key in the .env file
+    process.env.OPENAI_ASSISTANT_ID = assistantId;
+
+    console.log("assistant created.");
+    console.log("assistantId:", assistantId);
+  }
+  return assistantId
+};

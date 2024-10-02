8ee8986aa931b7a927d357727ed81798b368ebe7
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-01 23:29:00 -0600

    skeleton for updating an assistant

--- src/api/assistant/assistant.ts
+++ src/api/assistant/assistant.ts
@@ -83,3 +83,21 @@ export async function POST() {
   return Response.json({ assistantId: assistant.id });
 };
 
+// Update an existing assistant
+// export async function PATCH(request:any) { 
+//   // const files = await getAllRepoFiles(); // gwt the list of repo files
+//   const file = get("file"); // retrieve the single file from FormData
+//   const vectorStoreId = await getOrCreateVectorStore(); // get or create vector store
+
+//   // upload using the file stream
+//   const openaiFile = await openai.files.create({
+//     file: file,
+//     purpose: "assistants",
+//   });
+
+//   // add file to vector store
+//   await openai.beta.vectorStores.files.create(vectorStoreId, {
+//     file_id: openaiFile.id,
+//   });
+//   return new Response();
+// }

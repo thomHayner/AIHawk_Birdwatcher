71e7a38e12ab47886b60e68589cea75134fc6ce9
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-03 15:05:15 -0600

    separate 'config' from API

--- src/AiApi/assistant/assistant.ts
+++ src/AiApi/assistant/assistant.ts
@@ -1,84 +1,13 @@
 import { openai } from "../../utils/clients/openai.js";
-import { assistantModel, assistantName, assistantSystemPrompt } from "../../utils/clients/assistantConfig.js";
+import { assistantName, assistantModel, assistantSystemPrompt, assistantTools } from "../../utils/clients/assistantConfig.js";
 
 // Create a new assistant
 export async function POST() {
   const assistant = await openai.beta.assistants.create({
-    instructions: assistantSystemPrompt,
     name: assistantName,
     model: assistantModel,
-    tools: [
-      { type: "file_search" },
-      { type: "code_interpreter" },
-      {
-        type: "function",
-        function: {
-          name: "add_primary_label",
-          description: "Add a label to an Issue.",
-          parameters: {
-            type: "object",
-            properties: {
-              label: {
-                type: "string",
-                enum: ["bug", "documentation", "enhancement", "question"],
-              },
-            },
-            required: ["label"],
-          },
-        },
-      },
-      {
-        type: "function",
-        function: {
-          name: "add_duplicate_label",
-          description: "Add a label to an Issue.",
-          parameters: {
-            type: "object",
-            properties: {
-              label: {
-                type: "string",
-                enum: ["duplicate", "unique"],
-              },
-            },
-            required: ["label"],
-          },
-        },
-      },
-      {
-        type: "function",
-        function: {
-          name: "add_secondary_label",
-          description: "Add a label to an Issue.",
-          parameters: {
-            type: "object",
-            properties: {
-              label: {
-                type: "string",
-                enum: ["bug", "documentation", "enhancement", "question", "duplicate", "unique", "good first issue", "help wanted", "wontfix", "invalid", "intake"],
-              },
-            },
-            required: ["label"],
-          },
-        },
-      },
-      {
-        type: "function",
-        function: {
-          name: "remove_label",
-          description: "Remove a label from an Issue.",
-          parameters: {
-            type: "object",
-            properties: {
-              label: {
-                type: "string",
-                enum: ["bug", "documentation", "enhancement", "question", "duplicate", "unique", "good first issue", "help wanted", "wontfix", "invalid", "intake"],
-              },
-            },
-            required: ["label"],
-          },
-        },
-      },
-    ],
+    instructions: assistantSystemPrompt,
+    tools: assistantTools,
   });
   return Response.json({ assistantId: assistant.id });
 };

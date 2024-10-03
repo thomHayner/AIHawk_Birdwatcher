c12c4e3c902a15e8b8b82da938dd97d6a0708def
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-03 00:04:02 -0600

    cleanup, add types, init OpenAI 'uploads' to replace 'files'

--- src/utils/fileSearchLoader.ts
+++ src/utils/fileSearchLoader.ts
@@ -1,24 +1,47 @@
 import { octokit } from "./clients/octokit.js";
 import { openai } from "./clients/openai.js";
 import * as fs from "node:fs";
-import * as fsp from "node:fs/promises";
+import * as fsPromises from "node:fs/promises";
 import nodepath from "node:path";
 import findOrCreateVectorStore from "./clients/vectorStoreConfig.js";
-import { GET as getFileList, POST as addFileToVectorStore } from "../api/assistant/file_search/vector_store.js";
+import { GET as getVectorFileList, POST as addFileToVectorStore } from "../api/assistant/file_search/vector_store.js";
 
-///// This still needs a helper to replace updated duplicate files on a push/pr (to main only)
+// This still needs a helper to replace updated duplicate files on a push/pr (to main only)
 
-const supportedFileTypes:string[] = [".c", ".cpp", ".cs", ".css", ".doc", ".docx", ".go", ".html", ".java", ".js", ".json", ".md", ".pdf", ".php", ".pptx", ".py", ".rb", ".sh", ".tex", ".ts", ".txt"];
+type SupportedType = {
+  ext:string;
+  mime:string;
+}
+
+type RepoFile = {
+  path: string;
+  mode: string;
+  type: string;
+  sha: string;
+  size: number;
+  url: string;
+}
+
+type VectorStoreFile = {
+  file_id: string;
+  filename: string;
+  status: string;
+}
+
+// const supportedFileTypes:string[] = [".c", ".cpp", ".cs", ".css", ".doc", ".docx", ".go", ".html", ".java", ".js", ".json", ".md", ".pdf", ".php", ".pptx", ".py", ".rb", ".sh", ".tex", ".ts", ".txt"];
+const supportedTypes:SupportedType[] = [{ext: ".c",	mime: "text/x-c"},{ext: ".cpp",	mime: "text/x-c++"},{ext: ".cs",	mime: "text/x-csharp"},{ext: ".css",	mime: "text/css"},{ext: ".doc",	mime: "application/msword"},{ext: ".docx",	mime: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},{ext: ".go",	mime: "text/x-golang"},{ext: ".html",	mime: "text/html"},{ext: ".java",	mime: "text/x-java"},{ext: ".js",	mime: "text/javascript"},{ext: ".json",	mime: "application/json"},{ext: ".md",	mime: "text/markdown"},{ext: ".pdf",	mime: "application/pdf"},{ext: ".php",	mime: "text/x-php"},{ext: ".pptx",	mime: "application/vnd.openxmlformats-officedocument.presentationml.presentation"},{ext: ".py",	mime: "text/x-python"},{ext: ".py",	mime: "text/x-script.python"},{ext: ".rb",	mime: "text/x-ruby"},{ext: ".sh",	mime: "application/x-sh"},{ext: ".tex",	mime: "text/x-tex"},{ext: ".ts",	mime: "application/typescript"},{ext: ".txt",	mime: "text/plain"}];
 
 const vectorStoreId:string = await findOrCreateVectorStore();
 
 export default async function uploadAllRepoFilesToOpenAi():Promise<void> {
-  const repoFiles:any[] = await getRepoTreeRecursive(); // console.log("repo file list: ", repoFiles);
-  const vectorStoreFileList = await getFileList(); // console.log("vector file list: ", vectorStoreFileList);
+  const repoFiles:RepoFile[] = await getRepoTreeRecursive(); // console.log("repo file list: ", repoFiles);
+  const vectorStoreFileList: VectorStoreFile[] = await getVectorFileList();
+  console.log("vector file list: ", vectorStoreFileList);
 
   await Promise.all(repoFiles.map(async (file):Promise<any> => {
     const duplicate = await checkForDuplicateFile(vectorStoreFileList, file);
     if (duplicate) {
+      console.log(`${file.path} was skipped because it is already in the Vector Store`);
       return;
     } else {
       await fileDecodeAndUpload(file);
@@ -29,7 +52,7 @@ export default async function uploadAllRepoFilesToOpenAi():Promise<void> {
 
 export async function updateRepoFilesToOpenAiOnPush(pullRequestFiles:any[]):Promise<void> {
   // const pullRequestFiles:any[] = await getPullRequestFiles();
-  const vectorStoreFileList = await getFileList();
+  const vectorStoreFileList = await getVectorFileList();
 
   await Promise.all(pullRequestFiles.map(async (file:any):Promise<any> => {
     const duplicate = await checkForDuplicateFile(vectorStoreFileList, file);
@@ -47,12 +70,11 @@ export async function updateRepoFilesToOpenAiOnPush(pullRequestFiles:any[]):Prom
 /* Helper functions */
 
 // Check to see if the file is already in the Vector Store
-async function checkForDuplicateFile(vectorStoreFileList:any[], fileToAdd:any):Promise<any> {
-  if (vectorStoreFileList.includes(fileToAdd)) {
-    return true;
-  } else {
-    return false;
-  }
+async function checkForDuplicateFile(vectorStoreFileList:VectorStoreFile[], fileToAdd:RepoFile):Promise<boolean> {
+  return new Promise((resolve) => {
+    const exists = vectorStoreFileList.some((file:VectorStoreFile) => file.filename === fileToAdd.path);
+    resolve(exists);
+  });
 }
 
 // Gets a list with info of any repo files updated in a Pull Request
@@ -111,69 +133,143 @@ async function fileDecodeAndUpload(file:any):Promise<any> {
     return console.log("undefined file was skipped");
   }
 
-  const fileName:string = await fileNameIsolater(file.path);
-
-  // TODO: convert license to .txt - it already is a plin text file but might need .txt filetype appended
-  // if (file.path === "LICENSE") {
-  //   return console.log(`${file.path} was skipped because it is not supported in OpenAI Vector Stores`);
-  //   file.path += ".txt"
-  // }
+  // skip if file is empty
+  if (file.size === 0) {
+    return console.log(`${file.path} was skipped because the file size is 0`);
+  }
   
-  // TODO: convert gitignore to .txt - it already is a plin text file but might need .txt filetype appended
-  // if (file.path === ".gitignore") {
-  //   return console.log(`${file.path} was skipped because it is not supported in OpenAI Vector Stores`);
-  //   file.path += ".txt"
-  // }
-  
-  // check for un-supported filetypes
+  // skip if filetype is not supported by OpenAI
   const fileType:string = await fileTypeIsolater(file.path);
-  if (!supportedFileTypes.some(type => fileType === type)) {
+  if (!supportedTypes.some(type => fileType === type.ext)) {
     return console.log(`${file.path} was skipped because ${fileType} files are not supported in OpenAI Vector Stores`);
   }
+
+  const fileName:string = await fileNameIsolater(file.path);
 
   const fileContent:any = await octokit.repos.getContent({
     owner,
     repo,
     path: file.path,
   });
-
+  
   // decode the file from base64 to readable code
   const decodedContent:string = Buffer.from(fileContent.data.content, "base64").toString("utf-8");
   
-  // if it's a .yml or .yaml file, convert it (future)
-  // TODO: implement file converter to .txt or .json (.json preferred)
+  // skip if file is empty
+  if (decodedContent.length === 0) {
+    return console.log(`${file.path} was skipped because the file is empty`);
+  }
+  
+    // TODO: convert license to .txt - it already is a plin text file but might need .txt filetype appended
+    // if (file.path === "LICENSE") {
+    //   return console.log(`${file.path} was skipped because it is not supported in OpenAI Vector Stores`);
+    //   file.path += ".txt"
+    // }
+    
+    // TODO: convert gitignore to .txt - it already is a plin text file but might need .txt filetype appended
+    // if (file.path === ".gitignore") {
+    //   return console.log(`${file.path} was skipped because it is not supported in OpenAI Vector Stores`);
+    //   file.path += ".txt"
+    // }
+    
+    // TODO: implement file converter to .txt or .json (.json preferred)
+    // if it's a .yml or .yaml file, convert it (future)
 
-  // save the file locally (temporarily)
-  // let tempFilePath:string = nodepath.join("src", "temp", fileName);
   // if it's LICENSE or .gitignore, add .txt to the tempFile path (but not file.path, otherwise OctoKit wouldn't be able to find it)
   // if (fileName === "LICENSE" || fileName === ".gitignore") {
-  //   tempFilePath = nodepath.join("src", "temp", fileName+ ".txt");
-  //   await fsp.writeFile(tempFilePath, decodedContent);
-  // } else {
-  //   tempFilePath = nodepath.join("src", "temp", fileName);
-  //   await fsp.writeFile(tempFilePath, decodedContent);
-  // }
-  if (decodedContent.length === 0) {
-    return console.log(`${file.path} was skipped because it is empty`);
-  }
-
+    //   tempFilePath = nodepath.join("src", "temp", fileName+ ".txt");
+    //   await fsPromises.writeFile(tempFilePath, decodedContent);
+    // } else {
+      //   tempFilePath = nodepath.join("src", "temp", fileName);
+      //   await fsPromises.writeFile(tempFilePath, decodedContent);
+      // }
+      
+  // save the file locally (temporarily)
   const tempFilePath:string = nodepath.join("src", "temp", fileName);
-  await fsp.writeFile(tempFilePath, decodedContent);
+  await fsPromises.writeFile(tempFilePath, decodedContent);
 
-  // upload the file to OpenAI
-  const openaiFile:any = await openai.files.create({
-    file: fs.createReadStream(tempFilePath),
+  // await fsPromises.unlink(tempFilePath);
+  
+
+  // ////////// START: File Method (1) //////////
+  // // upload the file to OpenAI
+  // const openaiFile:any = await openai.files.create({
+  //   file: fs.createReadStream(tempFilePath),
+  //   purpose: "assistants",
+  // });
+
+  // // add file to vector store
+  // await openai.beta.vectorStores.files.create(vectorStoreId, {
+  //   file_id: openaiFile.id,
+  // });
+  
+  // // delete the temporary file
+  // await fsPromises.unlink(tempFilePath);
+  
+  // console.log(`Uploaded ${file.path} to OpenAI with file ID: ${openaiFile.id}`);
+  // ////////// END: File Method (1) //////////
+  
+  ////////// START: Upload Method (2) //////////
+  // define a helper to get the mime type
+  async function getMimeType(fileType: string): Promise<string> {
+    return new Promise((resolve) => {
+      const mimeTypeObj = supportedTypes.find((type) => type.ext === fileType);
+      resolve(mimeTypeObj.mime);
+    });
+  }
+  
+  // get the mime type of the file
+  const mimeType = await getMimeType(fileType);
+  // get the size of the file
+  const bytes = (await fsPromises.stat(tempFilePath)).size;
+  // create the secondary filename for OpenAI
+  const filename = file.path
+
+  // create the OpenAI upload for the file
+  const openaiUpload:any = await openai.uploads.create({
+    bytes: bytes,
+    filename: filename,
     purpose: "assistants",
+    mime_type: mimeType,
+  });
+
+  const openaiUploadId = openaiUpload.id;
+
+  // create the part for the upload
+  const openaiUploadPart:any = await openai.uploads.parts.create(
+    openaiUploadId,
+    {
+      data: fs.createReadStream(tempFilePath),
+    }
+  );
+
+  const openaiUploadPartId = openaiUploadPart.id;
+
+  // complete the upload
+  const openaiUploadCompletion:any = await openai.uploads.complete(
+    openaiUploadId,
+    {
+      part_ids: [openaiUploadPartId],
+    }
+  );
+
+  const fileId = openaiUploadCompletion.file.id;
+
+  // add file to vector store
+  await openai.beta.vectorStores.files.create(vectorStoreId, {
+    file_id: fileId,
   });
 
   // add file to vector store
   await openai.beta.vectorStores.files.create(vectorStoreId, {
-    file_id: openaiFile.id,
+    file_id: openaiUploadCompletion.file.id,
   });
   
   // delete the temporary file
-  await fsp.unlink(tempFilePath);
+  await fsPromises.unlink(tempFilePath);
   
-  console.log(`Uploaded ${file.path} to OpenAI with file ID: ${openaiFile.id}`);
+  console.log(`Uploaded ${file.path} to OpenAI with file ID: ${openaiUploadCompletion.id}`);
+  ////////// END: Upload Method (2) //////////
+  
   return new Response();
 }

8c55c4b057149f3f7e23257d32941cb2b4e90e62
Thomas <49766988+thomHayner@users.noreply.github.com>
2024-10-03 00:00:06 -0600

    cleanup, add functions to enable testing and initializer process

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Probot } from "probot";
 import issueIntake from "./AiCompletions/workflows/issueIntake.js";
 import issueCommentCreated from "./AiCompletions/workflows/issueWatcher.js";
-import { uploadAllRepoFilesToOpenAi } from "./AiAssistant/fileSearchLoader.js";
+import initializer from "./utils/initializerProcess/initializer.js";
+import findOrCreateAssistant from "./utils/clients/assistantConfig.js";
+import findOrCreateVectorStore from "./utils/clients/vectorStoreConfig.js";
+import uploadAllRepoFilesToOpenAi from "./utils/fileSearchLoader.js";
+import { deleteAllFromTesting, removeTempFiles } from "./api/files/testingFilesManagement.js";
 
 export default (app: Probot) => {
-
+  // initializer();
+  // findOrCreateAssistant();
+  // findOrCreateVectorStore();
   uploadAllRepoFilesToOpenAi();
+  // deleteAllFromTesting();
+  // removeTempFiles();
+
   /* 
   *  The first order of business will be to setup and configure the OpenAI Assistant and Vector Store
   *   1. Assistant
@@ -26,20 +35,20 @@ export default (app: Probot) => {
   // When a new issue is opened:
   app.on("issues.opened", async (context) => {
     // All new Issues go to the 'Issue Intake' workflow:
-    const issueIntakeResponse = await issueIntake(context)
+    const issueIntakeResponse = await issueIntake(context);
     await context.octokit.issues.createComment(issueIntakeResponse);
-  });
+  })
 
   // When an existing issue is commented on
   app.on("issue_comment.created", async (context) => {
     // If the comment was made by this bot, don't rely to self:
     // TODO: make the bot name in next line dynamic
     if (context.payload.sender.login === "ai-hawk-birdwatcher[bot]") {
-      return
+      return;
     }
     // If the issue is still in 'Intake', go to the 'Issue Intake' workflow:
     else if (context.payload.issue.labels.length > 0 && context.payload.issue.labels.map((n:any)=>n.name).includes("intake")) {
-      const issueIntakeResponse = await issueIntake(context)
+      const issueIntakeResponse = await issueIntake(context);
       await context.octokit.issues.createComment(issueIntakeResponse);
     }
     // Otherwise, go to normal comment workflow:
@@ -47,11 +56,11 @@ export default (app: Probot) => {
       const issueComment = await issueCommentCreated(context);
       await context.octokit.issues.createComment(issueComment);
     };
-  });
+  })
 
   app.on('push', async () => {
     uploadAllRepoFilesToOpenAi();
-  });
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
